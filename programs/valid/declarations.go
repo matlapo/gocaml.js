027ecd58836ab2e762ecfb141bb1446dcdec2aa1
Maxence Frenette <maxence.frenette@gmail.com>
2018-02-25 11:56:43 -0500

    Better testing framework !

---
+++ programs/valid/declarations.go
@@ -0,0 +1,9 @@
+package main
+
+var _, _, _ (int) = 1, 2, 3
+
+var (
+	a float64 = 12.4
+	b = 44
+	c, d int = b, 2
+)
---
+++ test.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const { spawnSync } = require('child_process');
+
+beforeAll(() => {
+    spawnSync('./build.sh');
+});
+
+testDirectory('programs/valid', (pathString) => {
+    const result = spawnSync('./run.sh', ['parse', pathString]);
+    const output = result.output.join('');
+    
+    expect(output).toBe('OK\n');
+    expect(result.status).toBe(0);
+});
+
+testDirectory('programs/parser', async (pathString) => {
+    const result = spawnSync('./run.sh', ['parse', pathString]);
+    const output = result.output.join('');
+
+    expect(output).toMatch(/Error/);
+    expect(result.status).toBe(1);
+});
+
+function testDirectory(pathString, testFunction) {
+    describe(pathString, () => {
+        for (const file of fs.readdirSync(pathString)) {
+            const subPath = pathString + path.sep + file;
+            if (isDir(subPath)) {
+                testDirectory(path.basename(subPath), testFunction);
+            } else {
+                it(path.basename(subPath), testFunction.bind(this, subPath));
+            }
+        }
+    });
+}
+
+function isDir(path) {
+    return fs.lstatSync(path).isDirectory();
+}
