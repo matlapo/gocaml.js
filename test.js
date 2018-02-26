0b5909501a75a78357f07944f8d24627880a927e
Maxence Frenette <maxence.frenette@gmail.com>
2018-02-26 13:43:47 -0500

    Refactored testing code

--- test.js
+++ test.js
@@ -8,39 +8,34 @@ beforeAll(() => {
     spawnSync('./build.sh');
 });
 
-testDirectory('programs/valid', (pathString) => {
-    const result = spawnSync('./run.sh', ['parse', pathString]);
-    const output = result.output.join('');
-    
-    expect(output).toMatch(/OK/);
-    expect(result.status).toBe(0);
-});
+testDirectory('programs/valid', 'parse', true);
+testDirectory('programs/parser', 'parse', false);
+testDirectory('programs/scanner', 'scan', false);
 
-testDirectory('programs/parser', async (pathString) => {
-    const result = spawnSync('./run.sh', ['parse', pathString]);
-    const output = result.output.join('');
-
-    expect(output).toMatch(/Error/);
-    expect(result.status).toBe(1);
-});
-
-testDirectory('programs/scanner', async (pathString) => {
-    const result = spawnSync('./run.sh', ['scan', pathString]);
-    const output = result.output.join('');
-
-    expect(output).toMatch(/Error/);
-    expect(result.status).toBe(1);
-});
-
-function testDirectory(pathString, testFunction) {
+function testDirectory(pathString, command, valid) {
     describe(path.basename(pathString), () => {
         for (const file of fs.readdirSync(pathString)) {
             const subPath = pathString + path.sep + file;
             if (isDir(subPath)) {
-                testDirectory(subPath, testFunction);
+                testDirectory(subPath, command, valid);
             } else {
-                it(path.basename(subPath), testFunction.bind(this, subPath));
+                testFile(subPath, command, valid)
             }
+        }
+    });
+}
+
+function testFile(pathString, command, valid) {
+    it(path.basename(pathString), () => {
+        const result = spawnSync('./run.sh', [command, pathString]);
+        const output = result.output.join('');
+    
+        if (valid) {
+            expect(output).toMatch(/OK/);
+            expect(result.status).toBe(0);
+        } else {
+            expect(output).toMatch(/Error/);
+            expect(result.status).toBe(1);
         }
     });
 }
