97f52aa7d3138591225a28136d13b047a54570de
Maxime Plante <maxime.pl@outlook.com>
2018-02-26 12:54:21 -0500

    Added test for type definitions and kind

---
+++ programs/valid/type_definition_dev.go
@@ -0,0 +1,21 @@
+
+package main
+
+type point struct {
+  x int
+  z, y [5]int
+  a struct {
+    x int
+    z, y [5]int
+  }
+}
+
+type array [5]int
+
+func test(x [5]int, y []int, a int) []int {
+  bla[5] = bla.fak[5].sadasfew
+}
+
+func main() {
+
+}
