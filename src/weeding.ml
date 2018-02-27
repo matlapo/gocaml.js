15091eaba5c4299ce3ed3b4fbfe013b7dde69120
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-02-27 11:12:41 -0500

    Simple statements and part of statements done for finding blanks in weeding

--- src/weeding.ml
+++ src/weeding.ml
@@ -1,5 +1,9 @@
 open Astwithposition
 open Utils
+open BatOption
+
+module Option = BatOption
+let bind x f = Option.bind f x
 
 let blank_s = "_"
 
@@ -13,10 +17,11 @@ let rec blank_def (t: typesDef) =
       || blank_def ts
     )
   | ArrayT (s, _) -> s = blank_s
-  | SliceT s -> s = blank_s
+  | SliceT (s, _) -> s = blank_s
 
-let rec blank_exp (e: exp) =
-  match e with
+
+let rec blank_exp (e: exp node) =
+  match e.value with
   | Id l ->
     l
     |> List.exists (function
@@ -24,32 +29,63 @@ let rec blank_exp (e: exp) =
       | _ -> false
     )
   | BinaryOp (_, (a, b)) ->
-    blank_exp a.value || blank_exp b.value
+    blank_exp a || blank_exp b
   | Unaryexp (_, a) ->
-    blank_exp a.value
+    blank_exp a
   | FuncCall (_, l) ->
-    List.exists (fun x -> blank_exp x.value) l
+    List.exists (fun x -> blank_exp x) l
   | Append (a, b) ->
-    blank_exp a.value || blank_exp b.value
+    blank_exp a || blank_exp b
   | _ -> false
 
-let rec blank_stm (s: stmt) =
-  match s with
+let rec blank_kind (k: kind) =
+  k
+  |> List.exists (fun x ->
+    match x with
+    | Variable s -> s = blank_s
+    | Array (s, l) ->
+      s = blank_s ||
+      l |> List.exists (fun x -> blank_exp x)
+  )
+
+let blank_simple (s: simpleStm node) =
+  match s.value with
+  | Assign (_, (l, e)) ->
+    List.exists blank_kind l
+    || List.exists blank_exp e
+  | ExpStatement e -> blank_exp e
+  | DoublePlus s -> s = blank_s
+  | DoubleMinus s -> s = blank_s
+  | ShortDeclaration (k, l) ->
+    List.exists blank_kind k
+    || List.exists blank_exp l
+  | Empty -> false
+
+let rec blank_stm (s: stmt node) =
+  match s.value with
+  | Block l ->
+    List.exists (fun x -> blank_stm x) l
   | Print l ->
-    List.exists (fun x -> blank_exp x.value) l
+    List.exists (fun x -> blank_exp x) l
   | Println l ->
-    List.exists (fun x -> blank_exp x.value) l
+    List.exists (fun x -> blank_exp x) l
   | Declaration l ->
     l
     |> List.exists (fun (s, _, es) ->
       es
-      |> List.exists (fun x -> blank_exp x.value)
+      |> List.exists (fun x -> blank_exp x)
     )
   | TypeDeclaration l ->
     l
     |> List.exists (fun (s, ts) ->
       s = blank_s || blank_def ts
     )
+  (* | If (s, e, l, el) ->
+      let a =
+        s
+        |> bind (fun x -> Some (blank_simple x))
+        |> Option.default false *)
+  | Simple s -> blank_simple s
   | _ -> false
 
 let illegal_blanks (prog: program) =
@@ -61,7 +97,6 @@ let illegal_blanks (prog: program) =
       l
       |> List.exists (fun (_, _, exps) ->
         exps
-        |> List.map (fun x -> x.value)
         |> List.exists (fun x -> blank_exp x)
       )
     | _ -> false
