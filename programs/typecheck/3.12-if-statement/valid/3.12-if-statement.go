9f5851567189948504c1ac14294c31d49da5176d
Maxime Plante <maxime.pl@outlook.com>
2018-03-13 13:59:45 -0400

    Added tests for 3.12

---
+++ programs/typecheck/3.12-if-statement/valid/3.12-if-statement.go
@@ -0,0 +1,22 @@
+package main;
+
+func main() {
+  var i string
+  if i := 0; 3 < i {
+    var i float64
+  }
+  if ; true {
+
+  }
+  if a := 0; true {
+    a = 1
+  } else {
+    a = 2
+  }
+  if b := 0; true {
+    b = 1
+  } else {
+    var b int
+    b = 2
+  }
+}
