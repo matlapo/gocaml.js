527f871f1e2981c07a196d898c83a87aa8df3625
Maxime Plante <maxime.pl@outlook.com>
2018-04-09 13:37:42 -0400

    Removed unecessary comment

--- src/ast.ml
+++ src/ast.ml
@@ -1,49 +1,4 @@
 
-
-(* type gotype =
-  | Defined of string * int * gotype
-  | Array of gotype * Int64.t
-  | Slice of gotype
-  | Struct of (string * gotype) list *)
-
-(*
-Will go up the scope.resolvedTypes to find the first float64.
-Then will add int * (type it found) to currentScope.resolvedTypes
-type int float64
-{
-  type myint int
-  var a myint
-}
-
-type myint int //0
-var b myint //0
-{
-  Will look in parents scope.resolvedTypes for 'myint', (find_scopedtype_of_string)
-  Then will set a binding to c with scopeid being the scope where myint is defined
-  var c myint //0
-  {
-    type myint int
-    var a myint
-    Will look at the scopedtype of c and b and see if they are the same.
-    Then will look in parent scopes to find the definition of the types to get the resolved type.
-    Then will see if they add. If they do add, create a tnode with the type being the scoped type of a or b
-    a = c + b
-  }
-}
-{
-
-}
-
-type a struct {
-  x,y int
-}
-type a int;
-{
-  type b a;
-}
-
-var y a *)
-
 type basetype =
   | BInt
   | BFloat64
