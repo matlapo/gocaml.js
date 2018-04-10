6ff3fecfaadaf23a57181b4b0e3d96c72f296aa8
Maxence Frenette <maxence.frenette@gmail.com>
2018-04-10 06:38:46 -0400

    Fixed zero values test

--- programs/codegen/zero-values.go
+++ programs/codegen/zero-values.go
@@ -1,5 +1,5 @@
 //~0
-//~0.0
+//~0
 //~
 
 package main
@@ -9,6 +9,6 @@ func main() {
 	var b float64
 	var c string
 	println(a)
-	println(b)
+	println(b) // Due to the go formatting rules, this will print 0 and not 0.0
 	println(c)
 }
