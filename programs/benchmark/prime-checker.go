0d53be10b684a828e1bcc348a459818b3cb9159d
Maxime Plante <maxime.pl@outlook.com>
2018-04-11 02:44:57 -0400

    prime checker works

--- programs/benchmark/prime-checker.go
+++ programs/benchmark/prime-checker.go
@@ -9,34 +9,35 @@ func confirmPrime(prime int) bool {
 	return true
 }
 
-var maybePrimes []int
+var maybePrimes [26]int
 
 func main() {
-	maybePrimes = append(maybePrimes, 964829)
-	maybePrimes = append(maybePrimes, 964861)
-	maybePrimes = append(maybePrimes, 964871)
-	maybePrimes = append(maybePrimes, 964879)
-	maybePrimes = append(maybePrimes, 964884)
-	maybePrimes = append(maybePrimes, 964889)
-	maybePrimes = append(maybePrimes, 964897)
-	maybePrimes = append(maybePrimes, 964913)
-	maybePrimes = append(maybePrimes, 964927)
-	maybePrimes = append(maybePrimes, 964933)
-	maybePrimes = append(maybePrimes, 964932)
-	maybePrimes = append(maybePrimes, 964967)
-	maybePrimes = append(maybePrimes, 964969)
-	maybePrimes = append(maybePrimes, 964973)
-	maybePrimes = append(maybePrimes, 964984)
-	maybePrimes = append(maybePrimes, 965023)
-	maybePrimes = append(maybePrimes, 965047)
-	maybePrimes = append(maybePrimes, 965059)
-	maybePrimes = append(maybePrimes, 965087)
-	maybePrimes = append(maybePrimes, 965086)
-	maybePrimes = append(maybePrimes, 965101)
-	maybePrimes = append(maybePrimes, 965113)
-	maybePrimes = append(maybePrimes, 965117)
-	maybePrimes = append(maybePrimes, 965131)
-	maybePrimes = append(maybePrimes, 965147)
+	maybePrimes[0] = 964827
+	maybePrimes[1] = 964829
+	maybePrimes[2] = 964861
+	maybePrimes[3] = 964871
+	maybePrimes[4] = 964879
+	maybePrimes[5] = 964884
+	maybePrimes[6] = 964889
+	maybePrimes[7] = 964897
+	maybePrimes[8] = 964913
+	maybePrimes[9] = 964927
+	maybePrimes[10] = 964933
+	maybePrimes[11] = 964932
+	maybePrimes[12] = 964967
+	maybePrimes[13] = 964969
+	maybePrimes[14] = 964973
+	maybePrimes[15] = 964984
+	maybePrimes[16] = 965023
+	maybePrimes[17] = 965047
+	maybePrimes[18] = 965059
+	maybePrimes[19] = 965087
+	maybePrimes[20] = 965086
+	maybePrimes[21] = 965101
+	maybePrimes[22] = 965113
+	maybePrimes[23] = 965117
+	maybePrimes[24] = 965131
+	maybePrimes[25] = 965147
 	for i:=0; i < 24; i++ {
 		if (confirmPrime(maybePrimes[i])) {
 			println("Number ", maybePrimes[i], " is prime.")
