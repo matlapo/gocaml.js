3e57aa4a840ac0a97df032d3a1a1ddc386a73d0c
Maxence Frenette <maxence.frenette@gmail.com>
2018-04-22 13:25:46 -0400

    Added grading spec

---
+++ test/grading.spec.js
@@ -0,0 +1,4 @@
+const { testDirectory, VALID, INVALID } = require('./util');
+
+testDirectory('grading/3-semantics+codegen/valid', 'codegen');
+//testDirectory('grading/3-extra+codegen/valid', 'codegen');
