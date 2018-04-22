24f2efb3ee24eb048929191f578528dff49d55b3
Maxence Frenette <maxence.frenette@gmail.com>
2018-04-22 14:07:26 -0400

    Ignored grading tests

--- test/grading.spec.js
+++ test/grading.spec.js
@@ -2,6 +2,8 @@ const { testDirectory, VALID, INVALID } = require('./util');
 
 testDirectory('grading/3-semantics+codegen/valid', 'codegen', {
     ignore: [
+        '8-9-return-array.go', // Typechecker
+        '8-9-return-slice.go', // Typechecker
         '9-6-callexpr-blank.go', // Weeder/Typechecker
         '9-6-callexpr.go', // Float precision
     ]
